0aa3bca976cdd9e611a0d3261d8eb9451d4a346d
leso205 <hugo.ruiz@yakabiz.com>
2026-05-11 13:48:48 +0200

    remove import test

--- src/tp_app/main.py
+++ src/tp_app/main.py
@@ -6,8 +6,6 @@ from pydantic import BaseModel
 from tp_app.calculator import add, divide, multiply, subtract
 from tp_app.text_tools import count_words, reverse, slugify
 
-import logging
-
 app = FastAPI(
     title="CNAM CI/CD Demo API",
     description="A simple API to showcase CI/CD with GitHub Actions and Render.",
